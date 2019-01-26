14a8b127bd3910f836c8e5fbbf8f11a402d6b8b7
linhaoran <1476589247@qq.com>
2019-01-26 19:05:31 +0800

    布局

--- lib/main.dart
+++ lib/main.dart
@@ -115,6 +115,7 @@
 import 'package:flutter/material.dart';
 import 'demo/drawer_demo.dart';
 import 'demo/bottom_navigation_bar_demo.dart';
+import 'demo/listview_demo.dart';
 
 void main() => runApp(MyApp());
 
@@ -170,7 +171,7 @@ class Home extends StatelessWidget{
         ),
         body: TabBarView(
           children: <Widget>[
-            Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
+            ListViewDemo(),
             Icon(Icons.change_history, size: 128.0, color: Colors.black12),
             Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
           ],
